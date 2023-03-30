1e0c2fca93b1fc6a03fbb08ac214b62d8b183059
erikaliane <105476284+erikaliane@users.noreply.github.com>
2023-03-30 11:45:18 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+
+const app= express()
+
+app.get('/', (req , res) => {
+    res.sendFile('./static/index.html', {
+        root: __dirname
+    })
+})
+
+
+
+
+app.use((req,res) => {
+    res.status(404).send('No se encontro tu pagina ...!!!')
+    //res.end('No se encontro tu pagina...!') - no reconoce etiqueta
+    //res.send('No se encontro')
+})
+
+app.listen(3000)
+console.log(`Server on port ${3000}`)
