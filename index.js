5a952654390c15db7be534e2598d19d08e64489b
ErikaVentura <105476284+erikaliane@users.noreply.github.com>
2023-03-30 23:23:50 -0500

    update

--- index.js
+++ index.js
@@ -2,17 +2,12 @@ const express = require('express')
 
 const app= express()
 
-app.get('/', (req , res) => {
-    res.send('<h3>Hello World! , Diseño de soluciones en la Nube, Erika Ventura Ramirez<h3>');  
+app.get('/about', (req , res) => {
+    res.send('Hello World! , Diseño de soluciones en la Nube, Erika Ventura Ramirez');  
    
 });
 
 
-app.use((req,res) => {
-    res.status(404).send('No se encontro tu pagina ...!!!')
-    //res.end('No se encontro tu pagina...!') - no reconoce etiqueta
-    //res.send('No se encontro')
-})
 
 app.listen(3000)
 console.log(`Server on port ${3000}`)
